ea628004fbb07d5199db574c2b106d892a388736
khushiiiii24 <khushisogani24@gmail.com>
2025-03-24 17:51:07 +0530

    third commit

---
+++ src/Login.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import '../src/Login.css'
+import { Link } from 'react-router-dom'
+function Login() {
+  return (
+    <div><div className="login-container">
+    <h2>Bill Splitter</h2>
+    <p>Split your bills easily</p>
+    <form action="#">
+        <div className="input-group">
+            <input type="email" placeholder="Enter your email" required />
+        </div>
+        <div className="input-group">
+            <input type="password" placeholder="Enter your password" required />
+        </div>
+        <button type="submit">Login</button>
+        <p><a href="#">Forgot Password?</a></p>
+    </form>
+    <p>Don't have an account? <Link to="/signUp">Sign Up</Link></p>
+    </div>
+</div>
+  )
+}
+
+export default Login
